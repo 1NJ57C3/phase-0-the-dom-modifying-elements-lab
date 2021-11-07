75277119b8be110b5bb5c1a183a92da9467d0b7d
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2021-11-06 23:57:37 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+// document.body.append(newHeader);
+// newHeader.classList.add("victory");
+newHeader.id = "victory";
+newHeader.innerHTML = "1NJ57C3 is the champion";
